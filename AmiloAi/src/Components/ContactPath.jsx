fcb9b1cdfada5587fe6a47a66c998489baf3476c
TKBiswalport <biswalt662@gmail.com>
2024-12-09 00:29:07 +0530

    All changes done in Contact Page

--- AmiloAi/src/Components/ContactPath.jsx
+++ AmiloAi/src/Components/ContactPath.jsx
@@ -7,45 +7,54 @@ const ContactPath = () => {
     return (
 
         <>
-            <div className='h-auto pl-24 pb-6 flex gap-10 '>
-                <div className=' justify-center items-center pb-28 w-3/5 h-full bg-blue-950'>
+            <div className='h-auto pl-40 pb-6 flex gap-10 '>
+                <div className=' justify-center items-center pl-16 pb-16 w-7/12 h-full bg-blue-950'>
                     
-                    <h2 className=' pl-16 pt-12  font-semibold text-xl text-white'>Send a Message</h2>
+                    <h2 className=' pl-16 pt-12 pb-10 font-semibold text-xl text-white'>Send a Message</h2>
                     <form action="" className='pl-16 pb-7'>
                         
-                        <table>
-                            <tr>
-                                
-                                <td><label className='text-white'>First Name</label> <br /> 
-                                <input type="text" placeholder='Enter First Name' className='border rounded-sm  ' /></td>
-                                
-                                <td><label className='text-white'>Last Name</label> <br />
-                                <input type="text" placeholder='Enter Last Name'  className='border rounded-sm'/></td>
-                            </tr>
-                            <tr>
-                                <td><label >Email</label><br />
-                                <input type="email" placeholder='Enter Email Address'  className='border rounded-sm'/></td>
-                                <td><label>Phone Number</label><br />
-                                <input type="number" placeholder='Enter Phone Number' className='border rounded-sm ' /></td>
-                            </tr>
-                            
-                        </table>
+                        <div className='flex gap-24 pb-4'>
+                            <div className='w-44 ' >
+                                <p className='text-white font-semibold text-lg pb-2'>First Name</p>
+                                <input type="text" className='w-52 h-8 pl-2 border border-gray ' placeholder='First Name' />
+                            </div>
 
-                        <label >Message</label>
-                        <div className=' w-2/5 h-52'>
+                            <div>
+                                <p className='text-white font-semibold text-lg pb-2'>Last Name</p>
+                                <input type="text" className='w-52 h-8 pl-2 border border-gray ' placeholder='Last Name' />
+                            </div>
+                            
+                        </div>
+
+                        <div className='flex gap-16 pb-4'>
+                            <div>
+                                <p className='text-white font-semibold text-lg pb-2'>Email</p>
+                                <input type="email" className='w-52 h-8 pl-2 border border-gray' placeholder='abc@gmail.com' />
+                            </div>
+                            <div> 
+                                <p className='text-white font-semibold text-lg pb-2'>Contact Number</p>
+                                <input type="contact" className='w-52 h-8 pl-2 border border-gray ' placeholder='Contact Number'/>
+                            </div>
+                        </div>
+
+                        <label className='text-white font-semibold text-lg '>Message</label>
+                        <div className=' w-2/5 h-52 pt-2'>
                             <textarea name="" id="message" cols={64} rows={8} placeholder='Write Your Message Here...'
                             autoComplete='off'></textarea>
                         </div>
-                        <button className='border py-2 px-6 text-sm font-bold bg-white '> Send</button>
+                        
+                        <div className=' pt-4'>
+                        <button className='border py-2 px-6  text- font-bold bg-white '> Send</button>
+                        </div>
 
                     </form>
                 </div>
   
 
                 <div>
-                    <div className='h-80 w-80 bg-blue-950'>
+                    <div className='h-96 w-80 bg-blue-950'>
                         <h2 className='pl-9 text-xl font-semibold text-white pt-12'>Contact Info</h2>
-                        <div className='flex pl-9 py-5'>
+                        <div className='flex pl-9 pt-14 py-5'>
                             <MdMyLocation  className='  border rounded-full text-2xl bg-white'/>
                             <p className='text-white font-semibold pl-4'>Bangalore</p>
                         </div>
@@ -60,7 +69,7 @@ const ContactPath = () => {
                     </div>
  
                     <div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.614603883637!2d77.68583057373115!3d12.932473815737609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae13b3907f1209%3A0x311a7b213162c6ab!2sJPMorgan%20Chase%20%26%20Co.!5e0!3m2!1sen!2sin!4v1733519544548!5m2!1sen!2sin" className=' pt-5 w-80 h-64'  loading="eager" referrerpolicy=""></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.614603883637!2d77.68583057373115!3d12.932473815737609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae13b3907f1209%3A0x311a7b213162c6ab!2sJPMorgan%20Chase%20%26%20Co.!5e0!3m2!1sen!2sin!4v1733519544548!5m2!1sen!2sin" className=' pt-5 w-80 h-72'  loading="eager" referrerpolicy=""></iframe>
                     </div>
                 </div>
             </div>
