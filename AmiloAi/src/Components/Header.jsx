00f0618b84ad9cf2441f12fcf3bbf313d1faf76a
sandeep-luhaniwal <sandeepluh0001@gmail.com>
2024-12-17 20:58:47 +0530

    about page done

--- AmiloAi/src/Components/Header.jsx
+++ AmiloAi/src/Components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
   return (
     <>
-        <header>
-            <div className='h-68 bg-blue-700 mb-8 p-20'>
-                <h3 className='text-6xl text-white font-extrabold'>{title}</h3>
-            </div>
-        </header>
+      <header className='bg-blue-600'>
+        <div className='max-w-[1240px] mx-auto px-4 xl:px-0 py-16 md:py-20 lg:py-24'>
+          <h1 className='text-5xl md:text-6xl text-white font-extrabold'>{title}</h1>
+        </div>
+      </header>
     </>
   )
 }
